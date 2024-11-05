3d0300036eb4f8046c05628d47250bbaaf508673
Lucas Carvalho Costa <teamlucas@aluno.ueg.br>
2024-11-04 21:30:31 -0300

    criação do service de book

---
+++ src/main/java/com/librarymanager/service/BookService.java
@@ -0,0 +1,40 @@
+package com.librarymanager.service;
+
+import com.librarymanager.model.Book;
+import com.librarymanager.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> listBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book updateBook(Long id, Book updatedBook) {
+        if (bookRepository.existsById(id)) {
+            updatedBook.setId(id);
+            return bookRepository.save(updatedBook);
+        }
+        return null;
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
